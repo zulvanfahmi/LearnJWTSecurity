cfdf06cb5034e486f802f2671bf81a57185e2e4a
Zulvan Fahmi <zulvanfahmi1612@gmail.com>
2025-03-11 22:02:55 +0700

    initial project

---
+++ src/main/java/com/LearnJWTSecurity/LearnJWTSecurity/LearnJwtSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.LearnJWTSecurity.LearnJWTSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJwtSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJwtSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LearnJWTSecurity/LearnJWTSecurity/LearnJwtSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LearnJWTSecurity.LearnJWTSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJwtSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
